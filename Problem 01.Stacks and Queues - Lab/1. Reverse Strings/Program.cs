77d465bb2d7f4d68380a592b8828ab408582bcf9
koko1912 <nmadzharov1912@gmail.com>
2022-04-10 15:57:47 +0300

    Add Task:Reverse Strings

---
+++ Problem 01.Stacks and Queues - Lab/1. Reverse Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            foreach (var charElement in input)
+            {
+                stack.Push(charElement);
+            }
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+
+            }
+
+        }
+    }
+}
+
