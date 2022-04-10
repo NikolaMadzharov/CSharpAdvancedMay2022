db0bf10674f2f1d350cb3dcd96c00f25cb4226b8
koko1912 <nmadzharov1912@gmail.com>
2022-04-10 16:00:18 +0300

    Add Task:Simple Calculator

---
+++ Problem 01.Stacks and Queues - Lab/3. Simple Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().Reverse().ToArray();
+            Stack<string> stack = new Stack<string>(input);
+            while (stack.Count > 1)
+            {
+                int firstNum = int.Parse(stack.Pop());
+                string operation = stack.Pop().ToString();
+                int secondNum = int.Parse(stack.Pop());
+                if (operation == "+")
+                {
+                    stack.Push((firstNum + secondNum).ToString());
+                }
+                else if (operation == "-")
+                {
+                    stack.Push((firstNum - secondNum).ToString());
+                }
+            }
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
