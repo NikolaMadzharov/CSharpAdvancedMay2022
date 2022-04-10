9b301b5ae32f69bf9c150dfbf806b13fd4f14d65
koko1912 <nmadzharov1912@gmail.com>
2022-04-10 16:03:07 +0300

    Add Task:Supermarket

---
+++ Problem 01.Stacks and Queues - Lab/6. Supermarket/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Queue<string> queue = new Queue<string>();
+            int count = 0;
+            while (input != "End")
+            {
+
+                if (input == "Paid")
+                {
+                    while (queue.Count > 0)
+                    {
+                        string name = queue.Dequeue();
+                        Console.WriteLine(name);
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+                input = Console.ReadLine();
+            }
+            int remaining = queue.Count;
+            Console.WriteLine($"{remaining} people remaining.");
+        }
+    }
+}
