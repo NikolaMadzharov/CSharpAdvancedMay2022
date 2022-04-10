3b100e196d314402070b4dbcfdc09a6f9098ea7a
koko1912 <nmadzharov1912@gmail.com>
2022-04-10 16:01:54 +0300

    Add Task:Matching Brackets

---
+++ Problem 01.Stacks and Queues - Lab/4. Matching Brackets/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            Stack<int> brackets = new Stack<int>();
+            for (int i = 0; i < command.Length; i++)
+            {
+                if (command[i].ToString() == "(")
+                {
+                    brackets.Push(i);
+                }
+                else if (command[i].ToString() == ")")
+                {
+                    int openBracketStart = brackets.Pop();
+                    Console.WriteLine(command.Substring(openBracketStart, i - openBracketStart + 1));
+                }
+
+            }
+        }
+    }
+}
