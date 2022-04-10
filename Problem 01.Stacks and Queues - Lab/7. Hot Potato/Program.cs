6d9d0693cc05947afa846912c7a629da1a822008
koko1912 <nmadzharov1912@gmail.com>
2022-04-10 16:03:29 +0300

    Add Task:Hot Potato

---
+++ Problem 01.Stacks and Queues - Lab/7. Hot Potato/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kids = Console.ReadLine().Split();
+            Queue<string> queue = new Queue<string>(kids);
+            int tossesLimit = int.Parse(Console.ReadLine());
+            int currentTosses = 1;
+            while (queue.Count > 1)
+            {
+                string currentKid = queue.Dequeue();
+                if (tossesLimit != currentTosses)
+                {
+                    queue.Enqueue(currentKid);
+                    currentTosses++;
+                }
+                else
+                {
+                    Console.WriteLine($"Removed {currentKid}");
+                    currentTosses = 1;
+
+                }
+
+            }
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
