edc8017a1ca909027e8c5d0ec5b112302506d9ed
koko1912 <nmadzharov1912@gmail.com>
2022-04-10 16:02:26 +0300

    Add Task:Print Even Numbers

---
+++ Problem 01.Stacks and Queues - Lab/5. Print Even Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(input);
+            for (int i = 0; i < input.Length; i++)
+            {
+                int currentElement = queue.Dequeue();
+                if (currentElement % 2 == 0)
+                {
+                    queue.Enqueue(currentElement);
+                }
+            }
+            Console.Write(String.Join(", ", queue));
+
+
+
+
+
+
+        }
+    }
+}
+
