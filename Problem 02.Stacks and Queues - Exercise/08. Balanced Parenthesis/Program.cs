3996eb7ae8f1d6c77d8b8620bd1bc95cdf87b2f5
koko1912 <nmadzharov1912@gmail.com>
2022-04-12 00:10:41 +0300

    Add Task:Balanced Parenthesis

---
+++ Problem 02.Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Balanced_Parenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            bool isValid = true;
+            foreach (var item in input)
+            {
+                if (item=='{' || item=='['||item=='(')
+                {
+                    stack.Push(item);
+                    continue;
+                }
+                if (stack.Count == 0)
+                {
+                    isValid = false;
+                    break;
+                }
+                if (item=='}'&& stack.Peek()=='{')
+                {
+                    stack.Pop();
+                }
+                else if (item==']'&& stack.Peek()=='[')
+                {
+                    stack.Pop();
+                }
+                else if (item==')'&& stack.Peek()=='(')
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (!isValid || stack.Count>0)
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
