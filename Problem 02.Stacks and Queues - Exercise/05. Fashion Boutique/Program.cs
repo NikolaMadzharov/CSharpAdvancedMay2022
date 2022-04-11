74b3d463da180e4c2996f71291e45fa795c1f420
koko1912 <nmadzharov1912@gmail.com>
2022-04-12 00:08:39 +0300

    Add Task:Fashion Boutique

---
+++ Problem 02.Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothes = Console.ReadLine()
+               .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+            Stack<int> stack = new Stack<int>(clothes);
+            int capacity = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int numberRacks = 1;
+
+            while (stack.Count > 0)
+            {
+                sum += stack.Peek();
+                if (sum <= capacity)
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    numberRacks++;
+                    sum = 0;
+                }
+
+            }
+            Console.WriteLine(numberRacks);
+        }
+    }
+}
