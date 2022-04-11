1a3fa728cab894003a4401c7419f576b8c01916a
koko1912 <nmadzharov1912@gmail.com>
2022-04-11 22:23:21 +0300

    Add Task:Maximum and Minimum Element

---
+++ Problem 02.Stacks and Queues - Exercise/03. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Maximum_and_Minimum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                int[] input =Console.ReadLine().Split().Select(int.Parse).ToArray();
+              
+
+                if (input[0] == 1)
+                {
+                    int elementToPush = input[1];
+                    stack.Push(elementToPush);
+                }
+                else if (input[0] == 2)
+                {
+                    if (stack.Count>0)
+                    {
+                        stack.Pop();
+                    }
+                }
+                else if (input[0] == 3)
+                {
+                    if (stack.Count>0)
+                    {
+                        Console.WriteLine(stack.Max());
+                    }
+                }
+                else if (input[0] == 4)
+                {
+                    if (stack.Count>0)
+                    {
+                        Console.WriteLine(stack.Min());
+                    }
+                }
+            }
+            Console.WriteLine(String.Join(", ",stack));
+         
+        }
+    }
+}
