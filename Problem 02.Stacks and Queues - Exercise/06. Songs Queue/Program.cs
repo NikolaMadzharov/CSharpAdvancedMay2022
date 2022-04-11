9dd95d112ae3a67a4f4d7d280724dafccc0a7d9e
koko1912 <nmadzharov1912@gmail.com>
2022-04-12 00:09:17 +0300

    Add Task:Songs Queue

---
+++ Problem 02.Stacks and Queues - Exercise/06. Songs Queue/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Songs_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             string [] songs = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries);
+            Queue<string> songQueue = new Queue<string>(songs);
+            while (songQueue.Count>0)
+            {
+                
+                string command = Console.ReadLine();
+                if (command=="Play")
+                {
+                    
+                    
+                        songQueue.Dequeue();
+                    
+                   
+                    
+
+                }
+                else if (command.StartsWith("Add"))
+                {
+                    string songFullname = command.Substring(4);
+                    
+                    
+                    if(songQueue.Contains(songFullname))
+                    {
+                        Console.WriteLine($"{songFullname} is already contained!");
+                    }
+                    else  
+                    {
+                        songQueue.Enqueue(songFullname);
+                    }
+                }
+                else if (command=="Show")
+                {
+                     Console.WriteLine("{0}", string.Join(", ",songQueue));
+                }
+            }
+            Console.WriteLine("No more songs!");
+
+        }
+    }
+}
