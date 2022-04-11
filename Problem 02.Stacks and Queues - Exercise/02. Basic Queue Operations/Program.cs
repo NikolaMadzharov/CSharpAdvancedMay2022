db5fb653ae638034be20f27721f4e9b0c0a34d9e
koko1912 <nmadzharov1912@gmail.com>
2022-04-11 22:22:35 +0300

    Add Task:Basic Queue Operations

---
+++ Problem 02.Stacks and Queues - Exercise/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] elements = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>();
+
+            int push = input[0];
+            int pop = input[1];
+            int specialElement = input[2];
+            for (int i = 0; i < push; i++)
+            {
+                queue.Enqueue(elements[i]);
+            }
+            for (int i = 0; i < pop; i++)
+            {
+                queue.Dequeue();
+            }
+            if (queue.Contains(specialElement))
+            {
+                Console.WriteLine("true");
+            }
+            else if (queue.Count > 0)
+            {
+                Console.WriteLine(queue.Min());
+            }
+            else
+            {
+                Console.WriteLine('0');
+            }
+
+        }
+    }
+}
