d0fa4717fb21ad51dcf9a5b3105b1f767d886677
koko1912 <nmadzharov1912@gmail.com>
2022-04-13 23:52:54 +0300

    Add Task:Sum Matrix Columns

---
+++ Problem 03.Multidimensional Arrays - Lab/2. Sum Matrix Columns/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Sum_Matrix_Columns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int []input = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            int[,]matrix = new int[rows, cols];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] inputinLoop = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = inputinLoop[col];
+                }
+            }
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int colSum = 0;
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    colSum+= matrix[row, col];
+                }
+                Console.WriteLine(colSum);
+            }
+          
+        }
+    }
+}
