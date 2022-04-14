e7bc38bf06f7da5f13cd5e6b38ef674de12af9f4
koko1912 <nmadzharov1912@gmail.com>
2022-04-14 21:48:00 +0300

    Add Task:Primary Diagonal

---
+++ Problem 03.Multidimensional Arrays - Lab/3. Primary Diagonal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _3._Primary_Diagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int[,]
+         }
+    }
+}
