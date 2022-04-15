e3b3012be7c5eb4b78762448768ce236f8d76be8
Nikola Madzharov <nmadzharov1912@gmail.com>
2022-04-16 00:06:37 +0300

    Add Task:Primary Diagonal

---
+++ Problem 03.Multidimensional Arrays - Lab/3. Primary Diagonal/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Primary_Diagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int[,] matrixx = new int [input, input];
+            for (int row = 0; row < matrixx.GetLength(0); row++)
+            {
+                int [] inputInLoop = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                for (int col = 0; col < input; col++)
+                {
+                    matrixx[row, col] = inputInLoop[col];
+                }
+            }
+            int sum = 0;
+            for (int i = 0; i <matrixx.GetLength(0); i++)
+            {
+                sum += matrixx[i, i];
+            }
+            Console.WriteLine(sum);
+                
+        }
+       
+    }
+}
