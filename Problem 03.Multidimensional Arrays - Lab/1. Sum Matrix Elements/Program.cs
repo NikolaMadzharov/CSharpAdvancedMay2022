762515d65e476e8534a7fbd11e7784c9a65d51be
Nikola Madzharov <nmadzharov1912@gmail.com>
2022-04-16 00:05:03 +0300

    Add Task:Sum Matrix Elements

---
+++ Problem 03.Multidimensional Arrays - Lab/1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Sum_Matrix_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int[]input = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            int[,]matrix = new int[rows, cols];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] inputInLoop = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int col = 0; col <matrix.GetLength(1) ; col++)
+                {
+                    matrix[row, col] = inputInLoop[col];
+                }
+            }
+            int sum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    sum+= matrix[row, col];
+                }
+            }
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+        }
+    }
+}
