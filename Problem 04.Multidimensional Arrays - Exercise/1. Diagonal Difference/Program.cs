e4b5bc81f4a29018458c2be9fe1365e7117679f9
Nikola Madzharov <nmadzharov1912@gmail.com>
2022-04-17 18:41:54 +0300

    Add Task:Diagonal Difference

---
+++ Problem 04.Multidimensional Arrays - Exercise/1. Diagonal Difference/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int size  = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size, size];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int [] numbersInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbersInput[col];
+                }
+            }
+            int firstDiagonalSum = 0;
+            int secondDiagonalSum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                firstDiagonalSum += matrix[i, i];
+            }
+            for (int i = 0; i <matrix.GetLength(0); i++)
+            {
+                secondDiagonalSum += matrix[i, matrix.GetLength(1) - 1 - i];
+            }
+            int result = firstDiagonalSum - secondDiagonalSum;
+            Console.WriteLine(Math.Abs(result));
+
+        }
+    }
+}
