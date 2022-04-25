041b50e5aabf5db5a657bbeb22dbdf60d6983bb0
nikola madzharov <nmadzharov1912@gmail.com>
2022-04-25 23:03:13 +0300

    add task:08. SoftUni Party

---
+++ Problem 05.Sets and Dictionaries Advanced - Lab/08. SoftUni Party/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._SoftUni_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> regular = new HashSet<string>();
+            HashSet<string> vip = new HashSet<string>();
+            string people = Console.ReadLine();
+            while(people != "PARTY")
+            {
+                if (char.IsDigit(people[0]))
+                {
+                    vip.Add(people);
+                }
+                else
+                {
+                    regular.Add(people);
+                }
+                people = Console.ReadLine();
+            }
+            string guests = Console.ReadLine();
+            while (guests!="END")
+            {
+                if (vip.Contains(guests))
+                {
+                    vip.Remove(guests);
+                }
+                else
+                {
+                    regular.Remove(guests);
+                }
+                guests = Console.ReadLine();
+            }
+           int  count = vip.Count + regular.Count;
+            Console.WriteLine(count);
+            if (vip.Count>0)
+            {
+                foreach (var guest in vip)
+                {
+                    Console.WriteLine(guest);
+                }
+            }
+            if (regular.Count>0)
+            {
+                foreach (var guest in regular)
+                {
+                    Console.WriteLine(guest);
+                }
+            }
+        }
+    }
+}
