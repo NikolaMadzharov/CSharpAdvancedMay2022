cfb6c8a354e39ea7d8ff14c1879b30c0eadeb315
nikola madzharov <nmadzharov1912@gmail.com>
2022-04-25 23:02:37 +0300

    add task:06. Record Unique Names

---
+++ Problem 05.Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfNames = int.Parse(Console.ReadLine());
+            HashSet<string> set = new HashSet<string>();
+            for (int i = 0; i < numberOfNames; i++)
+            {
+                string name = Console.ReadLine();
+                set.Add(name);
+            }
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
