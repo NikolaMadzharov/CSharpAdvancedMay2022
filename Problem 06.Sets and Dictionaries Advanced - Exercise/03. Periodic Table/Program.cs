9c5f507805403c3e41b285b87ac346f7479d0226
nikola madzharov <nmadzharov1912@gmail.com>
2022-04-26 22:14:56 +0300

    add task:03. Periodic Table

---
+++ Problem 06.Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int numberOfElelements = int.Parse(Console.ReadLine());
+         HashSet<string> elements = new HashSet<string>();
+            for (int i = 0; i < numberOfElelements; i++)
+            {
+                string[] chemicalElements = Console.ReadLine().Split(' ');
+                foreach (var element in chemicalElements)
+                {
+                    elements.Add(element);
+                }
+            }
+            HashSet<string> elements2 = elements.OrderBy(x=>x).ToHashSet();
+            foreach (var element in elements2)
+            {
+                Console.Write(element+" ");
+            }
+           
+        }
+    }
+}
