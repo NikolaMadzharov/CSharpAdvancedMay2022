f3c813eaf658a83c9bfcdbf842ff0c67218da6ae
nikola madzharov <nmadzharov1912@gmail.com>
2022-04-26 22:13:28 +0300

    add task:02. Sets of Elements

---
+++ Problem 06.Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int [] lenghtOfSets = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int firstSetLenght = lenghtOfSets[0];
+            HashSet<int> set = new HashSet<int>();
+
+            int secondSetLenght = lenghtOfSets[1];
+            HashSet<int> secondSet = new HashSet<int>();
+
+            fillingFirstSet(set,firstSetLenght);
+            fillingSecondSet(secondSet, secondSetLenght);
+            printingSets(set, secondSet);
+        }
+
+        private static void printingSets(HashSet<int> set, HashSet<int> secondSet)
+        {
+            foreach (var number in set)
+            {
+                if (secondSet.Contains(number))
+                {
+                    Console.Write(number+" ");
+                }
+            }
+        }
+
+        private static HashSet<int> fillingSecondSet(HashSet<int> secondSet, int secondSetLenght)
+        {
+            for (int i = 0; i < secondSetLenght; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                secondSet.Add(currentNumber);
+            }
+            return secondSet;
+        }
+
+        private static HashSet<int> fillingFirstSet(HashSet<int> set, int firstSetLenght)
+        {
+            for (int i = 0; i < firstSetLenght; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                set.Add(currentNumber);
+            }
+            return set;
+            
+        }
+    }
+}
