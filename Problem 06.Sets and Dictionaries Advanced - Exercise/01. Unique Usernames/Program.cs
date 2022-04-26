c23ce572d4c883b0d8a5fccc54c69d39ee7581ea
nikola madzharov <nmadzharov1912@gmail.com>
2022-04-26 22:13:11 +0300

    add task:01. Unique Usernames

---
+++ Problem 06.Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int numbersOfNames = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+            FillingSet(names,numbersOfNames);
+            printingNames(names);
+        }
+
+        private static HashSet<string> FillingSet(HashSet<string> set, int numbersOfNames)
+        {
+           
+            for (int i = 0; i < numbersOfNames; i++)
+            {
+                string name = Console.ReadLine();
+                set.Add(name);
+            }
+            return set;
+           
+        }
+
+        private static void printingNames(HashSet<string> set)
+        {
+           
+            foreach (var name in set)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
