32c1cdcbb928aa5b44f7c9ae4a0863bc36ce9c64
nikola madzharov <nmadzharov1912@gmail.com>
2022-05-03 22:27:56 +0300

    add tasK:02. Car Extension

---
+++ Problem 07. Defining Classes - Lab/02. Car Extension/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        string make;
+
+        string model;
+
+        int year;
+
+        double fuelQuantity;
+
+        double fuelConsumption;
+
+        public string Make { get { return this.make; } set { make = value; } }
+
+        public string Model { get { return this.model; } set { model = value; } }
+
+        public int Year { get { return this.year; } set { year = value; } }
+
+        public double FuelQuantity { get{ return this.fuelQuantity; } set { fuelQuantity = value; } }
+
+        public double FuelConsumption { get { return this.fuelConsumption; } set { fuelConsumption = value; } }
+        public void Drive (double distance)
+        {
+            if (fuelQuantity-distance* fuelConsumption > 0)
+            {
+                fuelQuantity -= distance * fuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip");
+            }
+        }
+        public   string WhoAMI()
+        {
+            return ($"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}");
+          
+        }
+    }
+}
