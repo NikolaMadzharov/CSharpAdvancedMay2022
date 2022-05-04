928fc54c0a53edb57ca63b02ad998cd93a862705
nikola madzharov <nmadzharov1912@gmail.com>
2022-05-04 22:13:11 +0300

    add task:01. Define a Class Person

---
+++ Problem 08.Defining Classes - Exercise/01. Define a Class Person/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        string name;
+        int age;
+
+        public string Name { get { return this.name; }  set{name=value;} }
+
+        public int Age { get { return this.age; } set {age=value; } }
+    }
+}
