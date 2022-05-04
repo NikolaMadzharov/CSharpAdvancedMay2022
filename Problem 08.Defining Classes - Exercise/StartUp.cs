388deeaa562ce245d5dcf2fabfc4c338c783ecda
nikola madzharov <nmadzharov1912@gmail.com>
2022-05-04 22:06:16 +0300

    add task:Defining Classes - Exercise

---
+++ Problem 08.Defining Classes - Exercise/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+           Person person = new Person();
+            person.Name = "Peter";
+            person.Age = 20;
+            Console.WriteLine($"Person Name: {person.Name}\nPerson Age:{person.Age}");
+        }
+    }
+}
