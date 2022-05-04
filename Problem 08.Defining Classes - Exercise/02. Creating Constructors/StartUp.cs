ef68c882f04bd34aeb8e23215a72449930a63a87
nikola madzharov <nmadzharov1912@gmail.com>
2022-05-04 22:15:43 +0300

    add task:02. Creating Constructors

---
+++ Problem 08.Defining Classes - Exercise/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+           Person person = new Person(20);
+          
+            Console.WriteLine($"Person Name: {person.Name}\nPerson Age:{person.Age}");
+        }
+    }
+}
