ea98dacb456f9e1288ab0f75a808dc9ed48e88d2
nikola madzharov <nmadzharov1912@gmail.com>
2022-05-08 15:50:24 +0300

    add task:03. Oldest Family Member

---
+++ Problem 08.Defining Classes - Exercise/03. Oldest Family Member/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+           Family family = new Family();
+            int members = int.Parse(Console.ReadLine());  
+
+            for (int i = 0; i < members; i++)
+            {
+                string [] input = Console.ReadLine().Split(' '); 
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person person = new Person(name,age);
+
+               family.AddMember(person);
+            }
+           Person oldest = family.GetOldestMember();
+            Console.WriteLine($"{oldest.Name} {oldest.Age}");
+        }
+    }
+}
