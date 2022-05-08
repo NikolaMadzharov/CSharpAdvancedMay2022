b5a3d7abf7af1cb95e5672a677a70a1898e4c295
nikola madzharov <nmadzharov1912@gmail.com>
2022-05-08 22:43:24 +0300

    add task:05. Date Modifier

---
+++ Problem 08.Defining Classes - Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        public static int differenceBetweenDates (string firstDate, string seconDate)
+            {
+            DateTime firstD= DateTime.Parse (firstDate);
+            DateTime seconD = DateTime.Parse(seconDate);
+            TimeSpan timeSpan = firstD - seconD;
+
+            int difference = (int)Math.Abs(timeSpan.TotalDays);
+            return difference;
+           
+            }
+    }
+}
---
+++ Problem 08.Defining Classes - Exercise/05. Date Modifier/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public  class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+            int result = DateModifier.differenceBetweenDates(firstDate, secondDate);
+            Console.WriteLine(result);
+        }
+    }
+}
