18594dd68d552b5b341ab5f72df2e43a5692bf7d
nikola madzharov <nmadzharov1912@gmail.com>
2022-05-08 15:50:45 +0300

    add task:03. Oldest Family Member

---
+++ Problem 08.Defining Classes - Exercise/03. Oldest Family Member/Family.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            People  = new List<Person>();
+        }
+        public List<Person> People { get; set; }
+        
+        public void AddMember(Person person)
+        {
+            People.Add(person);
+        }
+        public Person GetOldestMember()
+        {
+            Person person = People.OrderByDescending(x => x.Age).FirstOrDefault();
+
+            return person; ;
+        }
+    }
+    
+
+}
