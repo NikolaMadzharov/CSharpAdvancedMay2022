21b5034d2a24055ff3f14e2d9cefd3307dbd854b
nikola madzharov <nmadzharov1912@gmail.com>
2022-05-08 15:52:10 +0300

    add task:04. Opinion Poll

---
+++ Problem 08.Defining Classes - Exercise/04. Opinion Poll/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        string name;
+        int age;
+
+        
+        public Person(string name,int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get {return this.name; } set { name=value; } }
+        public int Age { get { return this.age; } set { age = value; } }
+    }
+}
---
+++ Problem 08.Defining Classes - Exercise/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            int countOfPeople = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < countOfPeople; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                people.Add(new Person(name, age));
+               
+
+            }
+            List<Person> peopleOver30 = people.Where(x => x.Age > 30).OrderBy(x => x.Name).ToList();
+            foreach (var person in peopleOver30)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
