ecac90656a04cc05c38fe241d3de80d37eeef06d
nikola madzharov <nmadzharov1912@gmail.com>
2022-05-10 22:19:10 +0300

    add task:06. Speed Racing

---
+++ Problem 08.Defining Classes - Exercise/06. Speed Racing/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06._Speed_Racing
+{
+    internal class Car
+    {
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            Travelleddistance = 0;
+           
+        }
+
+       
+      
+
+        public string Model { get; set; }
+
+        public double FuelAmount { get; set; }
+
+        public double FuelConsumptionPerKilometer { get; set; }
+
+        public double Travelleddistance { get; set; }
+
+        public void checkIfYoucanDriveCar(double amountOfKmToDrive)
+        {
+            double litters = amountOfKmToDrive * FuelConsumptionPerKilometer;
+            if (FuelAmount-litters>=0)
+            {
+                FuelAmount -= litters;
+                Travelleddistance += amountOfKmToDrive;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+      
+
+    }
+}
