6234f0208b65c188cfb5e9fc172cd2299e1b539e
nikola madzharov <nmadzharov1912@gmail.com>
2022-05-11 22:41:01 +0300

    add task:07. Raw Data

---
+++ Problem 08.Defining Classes - Exercise/07. Raw Data/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07._Raw_Data
+{
+    public  class Car
+    {
+        public Car(string мodel, Engine engine, Cargo cargo, Tires[] tire)
+        {
+            Мodel = мodel;
+            this.engine = engine;
+            this.cargo = cargo;
+            this.tire = tire;
+        }
+
+        public string Мodel { get; set; }
+
+        public Engine engine { get; set; }
+
+        public Cargo cargo { get; set; }
+
+        public Tires[] tire { get; set; }
+
+    }
+}
