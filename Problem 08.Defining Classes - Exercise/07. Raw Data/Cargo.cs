e5e1f7bae3f8532260d7c30ea7f5bce8c6199eb8
nikola madzharov <nmadzharov1912@gmail.com>
2022-05-11 22:40:46 +0300

    add task:07. Raw Data

---
+++ Problem 08.Defining Classes - Exercise/07. Raw Data/Cargo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07._Raw_Data
+{
+    public class Cargo
+    {
+        public Cargo(string type, int weight)
+        {
+            Type = type;
+            Weight = weight;
+        }
+
+        public string Type { get; set; }
+
+        public int Weight { get; set; }
+    }
+}
