187198ee5ba542cb32c31be7a1a2a1f449a94264
nikola madzharov <nmadzharov1912@gmail.com>
2022-05-12 22:04:21 +0300

    add task:03. Count Uppercase Words

---
+++ Problem 09.Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            Func<string, bool> isUpperLetter = w=>char.IsUpper(w[0]);
+            Console.WriteLine(String.Join(" ",input.Where(isUpperLetter)));
+        }
+    }
+}
