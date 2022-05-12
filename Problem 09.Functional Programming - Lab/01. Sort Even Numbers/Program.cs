dcfc7d852b647073ffb09a941e541d6310330a59
nikola madzharov <nmadzharov1912@gmail.com>
2022-05-12 22:03:27 +0300

    add task: Sort Even Numbers

---
+++ Problem 09.Functional Programming - Lab/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            numbers=numbers.Where(x=>x%2==0).OrderBy(x=>x).ToArray();
+
+            Console.WriteLine(String.Join(", ",numbers));
+
+
+        }
+    }
+}
