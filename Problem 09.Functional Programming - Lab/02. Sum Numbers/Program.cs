a93e34c32e1a988a20c4ec5fb87601f1d36fa6fe
nikola madzharov <nmadzharov1912@gmail.com>
2022-05-12 22:03:56 +0300

    add task:Sum Numbers

---
+++ Problem 09.Functional Programming - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+
+        }
+    }
+}
