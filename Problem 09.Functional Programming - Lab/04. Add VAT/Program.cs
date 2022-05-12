31ade41429342bc1f48538fbd6d7e6a82efa0c2f
nikola madzharov <nmadzharov1912@gmail.com>
2022-05-12 22:04:39 +0300

    add task:04. Add VAT

---
+++ Problem 09.Functional Programming - Lab/04. Add VAT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] numbers = Console.ReadLine().Split(", ").Select(decimal.Parse).ToArray();
+            numbers = numbers.Select(x=>x +x*0.2m).ToArray();
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number:f2}");
+            }
+        }
+    }
+}
