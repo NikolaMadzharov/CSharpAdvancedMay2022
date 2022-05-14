6a5a212424c7b87e5ad1036eab1f92b2758bb5bb
nikola madzharov <nmadzharov1912@gmail.com>
2022-05-14 22:34:35 +0300

    add task

---
+++ Problem 08.Defining Classes - Exercise/03. Oldest Family Member/Persons.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        string name;
+        int age;
+
+        public Person()
+        {
+            name = "No name";
+            age = 1;
+        }
+
+        public Person(int age):this()
+        {
+            Age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get { return this.name; }  set{name=value;} }
+
+        public int Age { get { return this.age; } set {age=value; } }
+    }
+}
