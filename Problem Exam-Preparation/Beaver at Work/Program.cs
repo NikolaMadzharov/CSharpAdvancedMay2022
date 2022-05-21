0910e113c93ecdc333351c37f582389ccdb8f882
koko1912 <89745007+koko1912@users.noreply.github.com>
2022-05-21 09:26:23 +0300

    100/100

--- Problem Exam-Preparation/Beaver at Work/Program.cs
+++ Problem Exam-Preparation/Beaver at Work/Program.cs
@@ -1,44 +1,44 @@
 using System;
 using System.Collections.Generic;
-
+ 
 namespace TruffleHunter
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+ 
             int n = int.Parse(Console.ReadLine());
             string[,] matrix = new string[n, n];
             int countBlackTruffles = 0;
             int countSummerTruffles = 0;
             int countWhiteTruffles = 0;
-
-
+ 
+ 
             int wildBoarEatenTruffles = 0;
-
+ 
             for (int row = 0; row < matrix.GetLength(0); row++)
             {
                 string[] fillMatrix = Console.ReadLine().Split(' ');
                 for (int col = 0; col < matrix.GetLength(1); col++)
                 {
                     matrix[row, col] = fillMatrix[col];
-
-
+ 
+ 
                 }
             }
             string cmd = Console.ReadLine();
-
+ 
             while (cmd != "Stop the hunt")
             {
                 string[] input = cmd.Split();
                 string action = input[0];
                 int row = int.Parse(input[1]);
                 int col = int.Parse(input[2]);
-
+ 
                 if (action == "Collect")
                 {
-
+ 
                     if (matrix[row, col] == "B")
                     {
                         countBlackTruffles += 1;
@@ -57,11 +57,11 @@ namespace TruffleHunter
                 }
                 else if (action == "Wild_Boar")
                 {
-
+ 
                     string direction = input[3];
-                   
-                   
-                    
+ 
+ 
+ 
                     if (direction == "up")
                     {
                         for (int i = row ; i >= 0; i-=2)
@@ -81,14 +81,14 @@ namespace TruffleHunter
                                 matrix[i, col] = "-";
                                 wildBoarEatenTruffles++;
                             }
-
+ 
                         }
-                       
-
+ 
+ 
                     }
                     else if (direction=="down")
                     {
-                        for (int i = row; i < matrix.GetLength(0) - 1; i++)
+                        for (int i = row; i < matrix.GetLength(0); i += 2)
                         {
                             if (matrix[i, col] == "S")
                             {
@@ -109,14 +109,14 @@ namespace TruffleHunter
                     }
                     else if (direction=="right")
                     {
-                        for (int i = col; i < matrix.GetLength(1) - 1; i += 2)
+                        for (int i = col; i < matrix.GetLength(1); i += 2)
                         {
                             if (matrix[row, i] == "S")
                             {
                                 matrix[row, i] = "-";
                                 wildBoarEatenTruffles++;
                             }
-                            else if (matrix[i, col] == "B")
+                            else if (matrix[row, i] == "B")
                             {
                                 matrix[row, i] = "-";
                                 wildBoarEatenTruffles++;
@@ -149,26 +149,182 @@ namespace TruffleHunter
                             }
                         }
                     }
-
-
-
+ 
+ 
+ 
                 }
                 cmd = Console.ReadLine();
-                //// down
-                //for (int i = row; i < matrix.GetLength(0)-1; i++)
-                //{
-
-                //}
-                ////right 
-                //for (int i = col; i < matrix.GetLength(1)-1; i+=2)
-                //{
-
-                //}
-                //// left 
-                //for (int i = col ; i >= 0; i-=2)
-                //{
-
-                //}
+                
+            }
+            Console.WriteLine($"Peter manages to harvest {countBlackTruffles} black, {countSummerTruffles} summer, and {countWhiteTruffles} white truffles.");
+            Console.WriteLine($"The wild boar has eaten {wildBoarEatenTruffles} truffles.");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i,j]+" ",StringSplitOptions.RemoveEmptyEntries);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}using System;
+using System.Collections.Generic;
+ 
+namespace TruffleHunter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            int n = int.Parse(Console.ReadLine());
+            string[,] matrix = new string[n, n];
+            int countBlackTruffles = 0;
+            int countSummerTruffles = 0;
+            int countWhiteTruffles = 0;
+ 
+ 
+            int wildBoarEatenTruffles = 0;
+ 
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] fillMatrix = Console.ReadLine().Split(' ');
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = fillMatrix[col];
+ 
+ 
+                }
+            }
+            string cmd = Console.ReadLine();
+ 
+            while (cmd != "Stop the hunt")
+            {
+                string[] input = cmd.Split();
+                string action = input[0];
+                int row = int.Parse(input[1]);
+                int col = int.Parse(input[2]);
+ 
+                if (action == "Collect")
+                {
+ 
+                    if (matrix[row, col] == "B")
+                    {
+                        countBlackTruffles += 1;
+                        matrix[row, col] = "-";
+                    }
+                    else if (matrix[row, col] == "S")
+                    {
+                        countSummerTruffles += 1;
+                        matrix[row, col] = "-";
+                    }
+                    else if (matrix[row, col] == "W")
+                    {
+                         countWhiteTruffles+= 1;
+                        matrix[row, col] = "-";
+                    }
+                }
+                else if (action == "Wild_Boar")
+                {
+ 
+                    string direction = input[3];
+ 
+ 
+ 
+                    if (direction == "up")
+                    {
+                        for (int i = row ; i >= 0; i-=2)
+                        {
+                            if (matrix[i, col] == "S")
+                            {
+                                matrix[i, col] = "-";
+                                wildBoarEatenTruffles++;
+                            }
+                            else if (matrix[i,col]=="B")
+                            {
+                                matrix[i, col] = "-";
+                                wildBoarEatenTruffles++;
+                            }
+                            else if (matrix[i, col] == "W")
+                            {
+                                matrix[i, col] = "-";
+                                wildBoarEatenTruffles++;
+                            }
+ 
+                        }
+ 
+ 
+                    }
+                    else if (direction=="down")
+                    {
+                        for (int i = row; i < matrix.GetLength(0); i += 2)
+                        {
+                            if (matrix[i, col] == "S")
+                            {
+                                matrix[i, col] = "-";
+                                wildBoarEatenTruffles++;
+                            }
+                            else if (matrix[i, col] == "B")
+                            {
+                                matrix[i, col] = "-";
+                                wildBoarEatenTruffles++;
+                            }
+                            else if (matrix[i, col] == "W")
+                            {
+                                matrix[i, col] = "-";
+                                wildBoarEatenTruffles++;
+                            }
+                        }
+                    }
+                    else if (direction=="right")
+                    {
+                        for (int i = col; i < matrix.GetLength(1); i += 2)
+                        {
+                            if (matrix[row, i] == "S")
+                            {
+                                matrix[row, i] = "-";
+                                wildBoarEatenTruffles++;
+                            }
+                            else if (matrix[row, i] == "B")
+                            {
+                                matrix[row, i] = "-";
+                                wildBoarEatenTruffles++;
+                            }
+                            else if(matrix[row,i] == "W")
+                            {
+                                matrix[row, i] = "-";
+                                wildBoarEatenTruffles++;
+                            }
+                        }
+                    }
+                    else if(direction=="left")
+                    {
+                        for (int i = col; i >= 0; i -= 2)
+                        {
+                            if (matrix[row, i] == "S")
+                            {
+                                matrix[row, i] = "-";
+                                wildBoarEatenTruffles++;
+                            }
+                            else if (matrix[row, i] == "B")
+                            {
+                                matrix[row, i] = "-";
+                                wildBoarEatenTruffles++;
+                            }
+                            else if (matrix[row, i] == "W")
+                            {
+                                matrix[row, i] = "-";
+                                wildBoarEatenTruffles++;
+                            }
+                        }
+                    }
+ 
+ 
+ 
+                }
+                cmd = Console.ReadLine();
+                
             }
             Console.WriteLine($"Peter manages to harvest {countBlackTruffles} black, {countSummerTruffles} summer, and {countWhiteTruffles} white truffles.");
             Console.WriteLine($"The wild boar has eaten {wildBoarEatenTruffles} truffles.");
