7876b578230376ad24cada5db64b99d5786dc494
koko1912 <89745007+koko1912@users.noreply.github.com>
2022-05-23 23:54:31 +0300

    Update Program.cs

--- Problem Exam-Preparation/Beaver at Work/Program.cs
+++ Problem Exam-Preparation/Beaver at Work/Program.cs
@@ -1,341 +1,216 @@
 using System;
 using System.Collections.Generic;
- 
-namespace TruffleHunter
+using System.Linq;
+
+namespace Survivor
 {
     internal class Program
     {
         static void Main(string[] args)
         {
- 
             int n = int.Parse(Console.ReadLine());
-            string[,] matrix = new string[n, n];
-            int countBlackTruffles = 0;
-            int countSummerTruffles = 0;
-            int countWhiteTruffles = 0;
- 
- 
-            int wildBoarEatenTruffles = 0;
- 
+            char[,] matrix = new char[n, n];
+            Queue<char> queue = new Queue<char>();
+            int woodBranchCount = 0;
+            int beaverRow = -10;
+            int beaverCol = -10;
             for (int row = 0; row < matrix.GetLength(0); row++)
             {
-                string[] fillMatrix = Console.ReadLine().Split(' ');
+                char[] input = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
                 for (int col = 0; col < matrix.GetLength(1); col++)
                 {
-                    matrix[row, col] = fillMatrix[col];
- 
- 
+                    matrix[row, col] = input[col];
+                    if (matrix[row, col] != 'F' && matrix[row, col] != '-' && matrix[row, col] != 'B')
+                    {
+                        woodBranchCount++;
+                    }
+                    if (matrix[row, col] == 'B')
+                    {
+                        beaverRow = row;
+                        beaverCol = col;
+                    }
                 }
             }
+
+
             string cmd = Console.ReadLine();
- 
-            while (cmd != "Stop the hunt")
+            while (cmd != "end")
             {
-                string[] input = cmd.Split();
-                string action = input[0];
-                int row = int.Parse(input[1]);
-                int col = int.Parse(input[2]);
- 
-                if (action == "Collect")
+                if (woodBranchCount.Equals(0))
                 {
- 
-                    if (matrix[row, col] == "B")
+                    Console.WriteLine("The Beaver successfully collect {numberOfbranches} wood branches:");
+                    Console.WriteLine(String.Join(", ", queue));
+                    break;
+                }
+
+
+                if (cmd == "up")
+                {
+                    if (beaverRow - 1 >= 0 && beaverRow - 1 < matrix.GetLength(0))
                     {
-                        countBlackTruffles += 1;
-                        matrix[row, col] = "-";
+                        if (char.IsLower(matrix[beaverRow - 1, beaverCol]))
+                        {
+                            queue.Enqueue(matrix[beaverRow - 1, beaverCol]);
+                            woodBranchCount--;
+                            matrix[beaverRow, beaverCol] = '-';
+                            matrix[beaverRow - 1, beaverCol] = 'B';
+                            beaverRow--;
+                        }
+                        else if (matrix[beaverRow - 1, beaverCol] == 'F')
+                        {
+                            if (beaverRow - 1 == 0)
+                            {
+                                matrix[beaverRow - 1, beaverCol] = '-';
+                                matrix[beaverRow, beaverCol] = '-';
+                                matrix[matrix.GetLength(0) - 1, beaverCol] = 'B';
+                                beaverRow = matrix.GetLength(0) - 1;
+                            }
+                            else
+                            {
+                                matrix[beaverRow - 1, beaverCol] = '-';
+                                matrix[0, beaverCol] = 'B';
+                                beaverRow = 0;
+                            }
+
+                        }
                     }
-                    else if (matrix[row, col] == "S")
+                    else
                     {
-                        countSummerTruffles += 1;
-                        matrix[row, col] = "-";
+                        queue.Dequeue();
                     }
-                    else if (matrix[row, col] == "W")
+
+
+
+                }
+                else if (cmd == "down")
+                {
+                    if (beaverRow + 1 >= 0 && beaverRow + 1 < matrix.GetLength(0))
                     {
-                         countWhiteTruffles+= 1;
-                        matrix[row, col] = "-";
+                        if (char.IsLower(matrix[beaverRow + 1, beaverCol]))
+                        {
+                            queue.Enqueue(matrix[beaverRow + 1, beaverCol]);
+                            woodBranchCount--;
+                            matrix[beaverRow, beaverCol] = '-';
+                            matrix[beaverRow + 1, beaverCol] = 'B';
+                            beaverRow++;
+                        }
+                        else if (matrix[beaverRow + 1, beaverCol] == 'F')
+                        {
+                            if (beaverRow + 1 == matrix.GetLength(0) - 1)
+                            {
+                                matrix[beaverRow + 1, beaverCol] = '-';
+                                matrix[beaverRow, beaverCol] = '-';
+                                matrix[0, beaverCol] = 'B';
+                                beaverRow = 0;
+                            }
+                            else
+                            {
+                                matrix[beaverRow + 1, beaverCol] = '-';
+                                matrix[matrix.GetLength(0) - 1, beaverCol] = 'B';
+                                beaverRow = matrix.GetLength(0) - 1;
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        queue.Dequeue();
                     }
                 }
-                else if (action == "Wild_Boar")
+                else if (cmd == "right")
                 {
- 
-                    string direction = input[3];
- 
- 
- 
-                    if (direction == "up")
+                    if (beaverCol + 1 < matrix.GetLength(1))
                     {
-                        for (int i = row ; i >= 0; i-=2)
+                        if (char.IsLower(matrix[beaverRow, beaverCol + 1]))
                         {
-                            if (matrix[i, col] == "S")
-                            {
-                                matrix[i, col] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                            else if (matrix[i,col]=="B")
-                            {
-                                matrix[i, col] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                            else if (matrix[i, col] == "W")
-                            {
-                                matrix[i, col] = "-";
-                                wildBoarEatenTruffles++;
-                            }
- 
+                            queue.Enqueue(matrix[beaverRow, beaverCol + 1]);
+                            woodBranchCount--;
+                            matrix[beaverRow, beaverCol] = '-';
+                            matrix[beaverRow, beaverCol + 1] = 'B';
+                            beaverCol++;
                         }
- 
- 
-                    }
-                    else if (direction=="down")
-                    {
-                        for (int i = row; i < matrix.GetLength(0); i += 2)
+                        else if (matrix[beaverRow, beaverCol + 1] == 'F')
                         {
-                            if (matrix[i, col] == "S")
+                            if (beaverCol + 1 == matrix.GetLength(1) - 1)
                             {
-                                matrix[i, col] = "-";
-                                wildBoarEatenTruffles++;
+                                matrix[beaverRow, beaverCol] = '-';
+                                matrix[beaverRow, beaverCol + 1] = '-';
+                                matrix[beaverRow, 0] = 'B';
+                                beaverCol = 0;
                             }
-                            else if (matrix[i, col] == "B")
+                            else
                             {
-                                matrix[i, col] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                            else if (matrix[i, col] == "W")
-                            {
-                                matrix[i, col] = "-";
-                                wildBoarEatenTruffles++;
+                                matrix[beaverRow, beaverCol] = '-';
+                                matrix[beaverRow, beaverCol + 1] = '-';
+                                matrix[beaverRow, matrix.GetLength(1) - 1] = 'B';
+                                beaverCol = matrix.GetLength(0) - 1;
                             }
                         }
+
+
                     }
-                    else if (direction=="right")
+                    else
                     {
-                        for (int i = col; i < matrix.GetLength(1); i += 2)
-                        {
-                            if (matrix[row, i] == "S")
-                            {
-                                matrix[row, i] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                            else if (matrix[row, i] == "B")
-                            {
-                                matrix[row, i] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                            else if(matrix[row,i] == "W")
-                            {
-                                matrix[row, i] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                        }
+                        queue.Dequeue();
                     }
-                    else if(direction=="left")
-                    {
-                        for (int i = col; i >= 0; i -= 2)
-                        {
-                            if (matrix[row, i] == "S")
-                            {
-                                matrix[row, i] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                            else if (matrix[row, i] == "B")
-                            {
-                                matrix[row, i] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                            else if (matrix[row, i] == "W")
-                            {
-                                matrix[row, i] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                        }
-                    }
- 
- 
- 
                 }
+                else if (cmd == "left")
+                {
+                    
+                    if (beaverCol - 1 >=0)
+                    {
+                        if (char.IsLower(matrix[beaverRow, beaverCol - 1]))
+                        {
+                            queue.Enqueue(matrix[beaverRow, beaverCol - 1]);
+                            woodBranchCount--;
+                            matrix[beaverRow, beaverCol] = '-';
+                            matrix[beaverRow, beaverCol - 1] = 'B';
+                            beaverCol--;
+                        }
+                        else if (matrix[beaverRow, beaverCol - 1] == 'F')
+                        {
+                            if (beaverCol - 1 == 0)
+                            {
+                                matrix[beaverRow, beaverCol] = '-';
+                                matrix[beaverRow, beaverCol - 1] = '-';
+                                matrix[beaverRow, matrix.GetLength(1) - 1] = 'B';
+                                beaverCol = matrix.GetLength(1) - 1;
+                            }
+                            else
+                            {
+                                matrix[beaverRow, beaverCol] = '-';
+                                matrix[beaverRow, beaverCol - 1] = '-';
+                                matrix[beaverRow, 0] = 'B';
+                                beaverCol = 0;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        queue.Dequeue();
+                    }
+
+
+                   
+
+                }
+
                 cmd = Console.ReadLine();
-                
             }
-            Console.WriteLine($"Peter manages to harvest {countBlackTruffles} black, {countSummerTruffles} summer, and {countWhiteTruffles} white truffles.");
-            Console.WriteLine($"The wild boar has eaten {wildBoarEatenTruffles} truffles.");
+            if (queue.Count == 0)
+            {
+                Console.WriteLine($"The Beaver failed to collect every wood branch. There are {woodBranchCount} branches left.");
+            }
+           
             for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < n; j++)
+                for (int z = 0; z < n; z++)
                 {
-                    Console.Write(matrix[i,j]+" ",StringSplitOptions.RemoveEmptyEntries);
-                }
-                Console.WriteLine();
-            }
-        }
-    }
-}using System;
-using System.Collections.Generic;
- 
-namespace TruffleHunter
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
- 
-            int n = int.Parse(Console.ReadLine());
-            string[,] matrix = new string[n, n];
-            int countBlackTruffles = 0;
-            int countSummerTruffles = 0;
-            int countWhiteTruffles = 0;
- 
- 
-            int wildBoarEatenTruffles = 0;
- 
-            for (int row = 0; row < matrix.GetLength(0); row++)
-            {
-                string[] fillMatrix = Console.ReadLine().Split(' ');
-                for (int col = 0; col < matrix.GetLength(1); col++)
-                {
-                    matrix[row, col] = fillMatrix[col];
- 
- 
-                }
-            }
-            string cmd = Console.ReadLine();
- 
-            while (cmd != "Stop the hunt")
-            {
-                string[] input = cmd.Split();
-                string action = input[0];
-                int row = int.Parse(input[1]);
-                int col = int.Parse(input[2]);
- 
-                if (action == "Collect")
-                {
- 
-                    if (matrix[row, col] == "B")
-                    {
-                        countBlackTruffles += 1;
-                        matrix[row, col] = "-";
-                    }
-                    else if (matrix[row, col] == "S")
-                    {
-                        countSummerTruffles += 1;
-                        matrix[row, col] = "-";
-                    }
-                    else if (matrix[row, col] == "W")
-                    {
-                         countWhiteTruffles+= 1;
-                        matrix[row, col] = "-";
-                    }
-                }
-                else if (action == "Wild_Boar")
-                {
- 
-                    string direction = input[3];
- 
- 
- 
-                    if (direction == "up")
-                    {
-                        for (int i = row ; i >= 0; i-=2)
-                        {
-                            if (matrix[i, col] == "S")
-                            {
-                                matrix[i, col] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                            else if (matrix[i,col]=="B")
-                            {
-                                matrix[i, col] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                            else if (matrix[i, col] == "W")
-                            {
-                                matrix[i, col] = "-";
-                                wildBoarEatenTruffles++;
-                            }
- 
-                        }
- 
- 
-                    }
-                    else if (direction=="down")
-                    {
-                        for (int i = row; i < matrix.GetLength(0); i += 2)
-                        {
-                            if (matrix[i, col] == "S")
-                            {
-                                matrix[i, col] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                            else if (matrix[i, col] == "B")
-                            {
-                                matrix[i, col] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                            else if (matrix[i, col] == "W")
-                            {
-                                matrix[i, col] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                        }
-                    }
-                    else if (direction=="right")
-                    {
-                        for (int i = col; i < matrix.GetLength(1); i += 2)
-                        {
-                            if (matrix[row, i] == "S")
-                            {
-                                matrix[row, i] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                            else if (matrix[row, i] == "B")
-                            {
-                                matrix[row, i] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                            else if(matrix[row,i] == "W")
-                            {
-                                matrix[row, i] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                        }
-                    }
-                    else if(direction=="left")
-                    {
-                        for (int i = col; i >= 0; i -= 2)
-                        {
-                            if (matrix[row, i] == "S")
-                            {
-                                matrix[row, i] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                            else if (matrix[row, i] == "B")
-                            {
-                                matrix[row, i] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                            else if (matrix[row, i] == "W")
-                            {
-                                matrix[row, i] = "-";
-                                wildBoarEatenTruffles++;
-                            }
-                        }
-                    }
- 
- 
- 
-                }
-                cmd = Console.ReadLine();
-                
-            }
-            Console.WriteLine($"Peter manages to harvest {countBlackTruffles} black, {countSummerTruffles} summer, and {countWhiteTruffles} white truffles.");
-            Console.WriteLine($"The wild boar has eaten {wildBoarEatenTruffles} truffles.");
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    Console.Write(matrix[i,j]+" ",StringSplitOptions.RemoveEmptyEntries);
+                    Console.Write(matrix[i,z]+" ");
                 }
                 Console.WriteLine();
             }
         }
     }
 }
+
