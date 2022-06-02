53a72a319313774d780e991975fa9c31c6b64da4
nikola madzharov <nmadzharov1912@gmail.com>
2022-06-02 23:22:43 +0300

    Have to repair 0/100

---
+++ Problem Exam-Preparation/Armory/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Armory
+{
+    internal class Program
+    {
+        private static char[,] matrix;
+        private static int firstMirrorRow;
+        private static int firstMirrorCol;
+
+        private static int secondMirrorRow;
+        private static int secondtMirrorCol;
+
+        private static int collectMoney;
+
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            matrix =  new char[size,size];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                
+                char[] input = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row,col] = input[col];
+                    if (matrix[row,col]=='M')
+                    {
+                        
+                    }
+                }
+            }
+        }
+    }
+}
