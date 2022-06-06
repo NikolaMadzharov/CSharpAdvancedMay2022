9dea4156ed2e557c15e49576a2d04a3416bf035f
nikola madzharov <nmadzharov1912@gmail.com>
2022-06-06 23:41:51 +0300

    91/100

---
+++ Problem Exam-Preparation/Bombs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bombs12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
