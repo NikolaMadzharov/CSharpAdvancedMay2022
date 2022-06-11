22d8be4e487b9441f5a107bebffb4666bc68ff6e
Nikola Madzharov <89745007+koko1912@users.noreply.github.com>
2022-06-11 23:17:33 +0300

    100/100

--- Problem 09.Functional Programming - Lab/04. Add VAT/Program.cs
+++ Problem 09.Functional Programming - Lab/04. Add VAT/Program.cs
@@ -7,8 +7,8 @@ namespace _04._Add_VAT
     {
         static void Main(string[] args)
         {
-            decimal[] numbers = Console.ReadLine().Split(", ").Select(decimal.Parse).ToArray();
-            numbers = numbers.Select(x=>x +x*0.2m).ToArray();
+            decimal[] number = Console.ReadLine().Split(", ").Select(decimal.Parse).ToArray();
+            numbers = number.Select(x=>x +x*0.2m).ToArray();
 
             foreach (var number in numbers)
             {
