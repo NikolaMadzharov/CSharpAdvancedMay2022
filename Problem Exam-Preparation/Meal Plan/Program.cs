2b0f8a11bdbe6e7b05601c36b3d3161bb8a11938
Nikola Madzharov <89745007+koko1912@users.noreply.github.com>
2022-06-16 23:07:13 +0300

    100/100

--- Problem Exam-Preparation/Meal Plan/Program.cs
+++ Problem Exam-Preparation/Meal Plan/Program.cs
@@ -1,62 +1,83 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Meal_Plan
+namespace MealPlan
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            string[] mealsInput = Console.ReadLine().Split(' ');
-            double[] inputCalories = Console.ReadLine().Split().Select(double.Parse).ToArray();
-           Stack<double> caloriesPerDay = new Stack<double>(inputCalories);
-            Queue<string> meals = new Queue<string>(mealsInput);
-            
-            while (caloriesPerDay.Count>0&& meals.Count>0)
+            int salad = 350;
+            int soup = 490;
+            int pasta = 680;
+            int steak = 790;
+
+            string[] meals = Console.ReadLine().Split();
+            Queue<string> queue = new Queue<string>(meals);
+            int[] calories = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(calories);
+
+            while (queue.Count != 0 && stack.Count != 0)
             {
-                string currentMeal = meals.Peek();
-                double currentCalories = caloriesPerDay.Pop();
-                if (currentMeal=="salad")
+                int meal = 0;
+                int calorie = stack.Peek();
+                if (queue.Peek() == nameof(salad))
                 {
-                    if (currentCalories>=350)
-                    {
-                        currentCalories-=  350;
-                       meals.Dequeue();
-                        
-                    }
+                    meal = salad;
                 }
-                else if (currentMeal=="soup")
+                else if (queue.Peek() == nameof(soup))
                 {
-                    if (currentCalories>=490)
-                    {
-                        currentCalories -= 490;
-                        meals.Dequeue();
-                    }
+                    meal = soup;
                 }
-                else if (currentMeal == "pasta")
+                else if (queue.Peek() == nameof(pasta))
                 {
-                    if (currentCalories >= 680)
-                    {
-                        currentCalories -= 680;
-                        meals.Dequeue();
-                    }
+                    meal = pasta;
                 }
-                else if (currentMeal == "steak")
+                else if (queue.Peek() == nameof(steak))
                 {
-                    if (currentCalories >= 790)
-                    {
-                        currentCalories -= 790;
-                        meals.Dequeue();
-                    }
+                    meal = steak;
                 }
-               
 
+                if (calorie < meal)
+                {
+                    stack.Pop();
+                    int difference =Math.Abs(calorie-meal);
+                   
+                    if (!stack.Any())
+                    {
+                        queue.Dequeue();
+                        break;
+                       
+                    }
+                    queue.Dequeue();
+                    
+                    stack.Push(stack.Pop()-difference);
+                    
+                }
+                else if (calorie >= meal)
+                {
+                    stack.Pop();
+                    calorie -= meal;
+                    stack.Push(calorie);
+                    queue.Dequeue();
+                }
             }
-
-
+            if (stack.Count == 0)
+            {
+                Console.WriteLine($"John ate enough, he had {meals.Length - queue.Count} meals.");
+                Console.WriteLine($"Meals left: {string.Join(", ", queue)}.");
             }
-           
+            if (queue.Count == 0)
+            {
+                //                "John had {number of meals} meals."
+                //"For the next few days, he can eat {dailyCalories1, dailyCalories2, etc.} calories."
+                Console.WriteLine($"John had {meals.Length} meals.");
+                Console.WriteLine($"For the next few days, he can eat {string.Join(", ", stack)} calories.");
+            }
         }
     }
- 
+}
+
+
+
