6ba406f5dca9110c10047607f43fef5411fbed0e
nikola madzharov <nmadzharov1912@gmail.com>
2022-06-16 23:06:17 +0300

    100/100

---
+++ Problem Exam-Preparation/Birthday Celebration/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Birthday_Celebration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] queueInput = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Queue<int> guests = new Queue<int>(queueInput);
+            int [] stackInput = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Stack <int> plates = new Stack<int>(stackInput);
+            int wastedFood = 0;
+            while (guests.Any()&& plates.Any())
+            {
+               int currentGuest = guests.Peek();
+                int currentPlate = plates.Peek();
+                if (currentPlate>=currentGuest)
+                {
+                    wastedFood += currentPlate - currentGuest;
+                    guests.Dequeue();
+                    plates.Pop();
+                    
+                }
+                else if (currentGuest>currentPlate)
+                {
+                   
+                    List<int> neededStack = new List<int>(guests);
+                    neededStack[0]-=currentPlate;
+                    guests = new Queue<int>(neededStack);
+                    plates.Pop();
+                  
+                }
+               
+
+            }
+            if (plates.Count!=0)
+            {
+                Console.WriteLine($"Plates: {string.Join(" ",plates)}");
+            }
+            if (guests.Count!=0)
+            {
+                Console.WriteLine($"Guests: {string.Join(" ",guests)}");
+            }
+            Console.WriteLine($"Wasted grams of food: {wastedFood}");
+
+        }
+    }
+}
