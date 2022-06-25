df503c5e128b60ff170d72c918533209efa6dbe0
nikola madzharov <nmadzharov1912@gmail.com>
2022-06-25 19:05:07 +0300

    93/100

---
+++ Problem 10.FinalExam 25 June/ThirdProblem/Renovator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Renovators
+{
+    public class Renovator
+    {
+        public Renovator(string name, string type, double rate, int days)
+        {
+            Name = name;
+            Type = type;
+            Rate = rate;
+            Days = days;
+        }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public double Rate { get; set; }
+        public int Days { get; set; }
+
+        public bool Hired { get; set;} = false;
+
+        public override string ToString()
+        {
+            StringBuilder info = new StringBuilder();
+            info.Append($"-Renovator: {this.Name}{Environment.NewLine}--Specialty: {this.Type}{Environment.NewLine}--Rate per day: {this.Rate} BGN");
+            return info.ToString();
+
+        }
+    }
+
+}
